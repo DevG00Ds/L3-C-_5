fa7a15ce6f69a851bd05692674049d165c85bda0
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-02-26 01:07:44 +0300

    fibish task 34

--- task34Home/Program.cs
+++ task34Home/Program.cs
@@ -4,16 +4,18 @@
 
 // [345, 897, 568, 238] -> 2
 
+
 Console.WriteLine("Введите размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[size];
 
-void GetRandomArray(int[] array)
+int[] GetRandomArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
         array[i] = new Random().Next(100, 1000);
     }
+    return array;
 }
 
 GetRandomArray(array);
